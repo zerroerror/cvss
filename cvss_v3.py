46e754c7bc441766c3087be7f285c7868df7ae64
zerroerror <66072039+zerroerror@users.noreply.github.com>
2020-09-20 18:09:40 +0500

    Update cvss_v3.py

--- cvss_v3.py
+++ cvss_v3.py
@@ -14,6 +14,8 @@ def Dengerlevel(BaseScore):
         print('High Danger Level')
     if (BaseScore>=9):
         print('Critical Danger Level')
+print('----For BaseMitric----')
+print('\n')
 AV=float(input('Please, input AV : \n'))
 AC=float(input('Please, input AC : \n'))
 PR=float(input('Please, input PR : \n'))
@@ -37,5 +39,46 @@ else:
         BaseScore=RoundUp(min(1.08*(Impact+Exploitability),10))
 print('-------------------\n')
 print('BaseScore = ',BaseScore)
-print('-------------------\n')
 Dengerlevel(BaseScore)
+print('-------------------\n')
+print('\n')
+print('----For TimeMitric----')
+print('\n')
+E=float(input('Please, input E : \n'))
+RL=float(input('Please, input RL : \n'))
+RC=float(input('Please, input RC : \n'))
+TemporalScore=RoundUp(BaseScore*E*RL*RC)
+print('-------------------\n')
+print('TimeMetric = ',TemporalScore)
+print('-------------------\n')
+print('\n')
+print('----For Contextualmetric----')
+print('\n')
+MAV=float(input('Please, input MAV : \n'))
+MAC=float(input('Please, input MAC : \n'))
+MPR=float(input('Please, input MPR : \n'))
+MUI=float(input('Please, input MUI : \n'))
+MS=float(input('Please, input MS : \n'))
+MC=float(input('Please, input MC : \n'))
+MI=float(input('Please, input MI : \n'))
+MA=float(input('Please, input MA : \n'))
+CR=float(input('Please, input CR : \n'))
+IR=float(input('Please, input IR : \n'))
+AR=float(input('Please, input AR : \n'))
+MExploitability=8.22*MAV*MAC*MPR*MUI
+MImpactBase=min((1-(1-MC*CR)*(1-MI*IR)*(1-MA*AR)), 0.915)
+if (MS==1):
+    MImpact=7.52*(MImpactBase-0.029)-3.25*(MImpactBase-0.02)**15
+else:
+    Mimpact=6.42*MImpactBase
+if (MImpact<0):
+    EnvironmentalScore=0
+else:
+    if (MS==0):
+        EnvironmentalScore=RoundUp(RoundUp(min(MImpact+MExploitability,10))*E*RL*RC)
+    else:
+        EnvironmentalScore=RoundUp(RoundUp(min(1.08*(MImpact+MExploitability),10))*E*RL*RC)
+print('-------------------\n')
+print('Contextualmetric = ',TemporalScore)
+print('-------------------\n')
+print('\n')
